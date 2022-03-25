385e1fcc016147f551a98255bb3438009a62a45e
Victor Mendoza <victormendoza@animum3d.com>
2022-03-25 03:17:22 +0100

    He modificado el controller para que funcione aunque no sigue y pueda romper la arquitectura hexagonal

--- app/Controller/Devices/DevicesPutController.php
+++ app/Controller/Devices/DevicesPutController.php
@@ -4,18 +4,29 @@ namespace App\Controller\Devices;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Vic\BackOffice\Devices\Application\DeviceCreator;
+use Vic\BackOffice\Devices\Application\CreateDeviceCommand;
+use Vic\BackOffice\Devices\Application\CreateDeviceCommandHandler;
+use Vic\BackOffice\Devices\Infrastructure\Persistence\DoctrineDeviceRepository;
 
 class DevicesPutController
 {
 
     public function __invoke(string $id, Request $request): Response
     {
-        $name = $request->get('name');
-        $mac_address = $request->get('mac_address');
-        
-        // TODO faltaria el aplication service para guardar los datos
 
-        return new Response('', Response::HTTP_CREATED);
+        // TODO Este código no es correcto ya que debería ejecutarse de otra manera
+        $device_creator = new DeviceCreator(new DoctrineDeviceRepository);
+
+        $create_device_command_handler = new CreateDeviceCommandHandler($device_creator);
+
+        $create_device_command_handler->__invoke(new CreateDeviceCommand(
+                                            $id,
+                                            $request->get('name'),
+                                            $request->get('mac_address'),
+                                        ));
         
+        return new Response('', Response::HTTP_CREATED);
+
     }
 }
