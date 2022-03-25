59bdd380297c3fa06b72334f9aa3463e70454cf8
Victor Mendoza <victormendoza@animum3d.com>
2022-03-25 03:15:43 +0100

    He quitado el use del DeviceId que ya no se utiliza

--- src/BackOffice/Devices/Infrastructure/Persistence/DoctrineDeviceRepository.php
+++ src/BackOffice/Devices/Infrastructure/Persistence/DoctrineDeviceRepository.php
@@ -6,17 +6,13 @@ namespace Vic\BackOffice\Devices\Infrastructure\Persistence;
 
 use Vic\BackOffice\Devices\Domain\Device;
 use Vic\BackOffice\Devices\Domain\DeviceRepository;
-use Vic\BackOffice\Devices\Domain\DeviceId;
+use Vic\BackOffice\Devices\Domain\ValueObjet\DeviceId;
+use Vic\Shared\Infrastructure\Persistence\Doctrine\DoctrineRepository;
 
 final class DoctrineDeviceRepository extends DoctrineRepository implements DeviceRepository
 {
     public function save(Device $device): void
     {
         $this->persist($device);
-    }
-
-    public function search(DeviceId $id): ?Device
-    {
-        return $this->repository(Device::class)->find($id);
     }
 }
