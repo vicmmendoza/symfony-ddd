e760f5eb8ed4c4ffa82ac3935493e011a05dc51e
Victor Mendoza <victormendoza@animum3d.com>
2022-03-25 01:53:06 +0100

    Interface de repository para Device

---
+++ src/BackOffice/Devices/Infrastructure/Persistence/DoctrineDeviceRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vic\BackOffice\Devices\Infrastructure\Persistence;
+
+use Vic\BackOffice\Devices\Domain\Device;
+use Vic\BackOffice\Devices\Domain\DeviceRepository;
+use Vic\BackOffice\Devices\Domain\DeviceId;
+
+final class DoctrineDeviceRepository extends DoctrineRepository implements DeviceRepository
+{
+    public function save(Device $device): void
+    {
+        $this->persist($device);
+    }
+
+    public function search(DeviceId $id): ?Device
+    {
+        return $this->repository(Device::class)->find($id);
+    }
+}
