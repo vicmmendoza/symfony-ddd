a93a8a6ec429c83b93a4b3fef5d5ebd2714dc872
Victor Mendoza <victormendoza@animum3d.com>
2022-03-25 03:14:48 +0100

    He borrado un metodo que ahora mismo no se va a implementar

--- src/BackOffice/Devices/Domain/DeviceRepository.php
+++ src/BackOffice/Devices/Domain/DeviceRepository.php
@@ -3,11 +3,8 @@
 namespace Vic\BackOffice\Devices\Domain;
 
 use Vic\BackOffice\Devices\Domain\Device;
-use Vic\BackOffice\Devices\Domain\ValueObject\DeviceId;
 
 interface DeviceRepository
 {
     public function save(Device $device): void;
-
-    public function search(DeviceId $id): ?Device;
 }
