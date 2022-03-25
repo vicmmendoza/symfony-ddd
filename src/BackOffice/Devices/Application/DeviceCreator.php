a53a984779a70e208320dea475ab0280143f8185
Victor Mendoza <victormendoza@animum3d.com>
2022-03-25 01:55:25 +0100

    Crear un registro en la base de datos

---
+++ src/BackOffice/Devices/Application/DeviceCreator.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vic\BackOffice\Devices\Application;
+
+use Vic\BackOffice\Devices\Domain\Device;
+use Vic\BackOffice\Devices\Domain\DeviceRepository;
+use Vic\BackOffice\Devices\Domain\ValueObject\DeviceId;
+use Vic\BackOffice\Devices\Domain\ValueObject\DeviceName;
+use Vic\BackOffice\Devices\Domain\ValueObject\DeviceMacAddress;
+
+
+final class DeviceCreator
+{
+
+    private $repository;
+
+    public function __construct(DeviceRepository $repository)
+    {
+    }
+
+    public function __invoke(DeviceId $id, DeviceName $name, DeviceMacAddress $mac_address): void
+    {
+        $Device = Device::create($id, $name, $mac_address);
+        
+        $this->repository->save($Device);
+
+        // TODO codear eventos
+    }
+}
