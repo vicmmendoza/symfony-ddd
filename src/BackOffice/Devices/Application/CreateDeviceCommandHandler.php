7cfe5a11f4fc800b8c43d49b780792d531849b69
Victor Mendoza <victormendoza@animum3d.com>
2022-03-25 01:56:08 +0100

    Transformador de Devices

---
+++ src/BackOffice/Devices/Application/CreateDeviceCommandHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vic\BackOffice\Devices\Application;
+
+use Vic\BackOffice\Devices\Application\DeviceCreator;
+use Vic\BackOffice\Devices\Domain\ValueObject\DeviceId;
+use Vic\BackOffice\Devices\Domain\ValueObject\DeviceName;
+use Vic\BackOffice\Devices\Domain\ValueObject\DeviceMacAddress;
+
+
+final class CreateDeviceCommandHandler
+{
+
+    private DeviceCreator $creator;
+
+    public function __construct(DeviceCreator $creator)
+    {
+    }
+
+    public function __invoke(CreateDeviceCommand $command): void
+    {
+        $id             = new DeviceId($command->id());
+        $name           = new DeviceName($command->name());
+        $mac_address    = new DeviceMacAddress($command->mac_address());
+
+        //$this->creator->__invoke($id, $name, $mac_address);
+    }
+}
