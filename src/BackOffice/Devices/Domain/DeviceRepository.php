e311de305cf61338c5c3e39ec4078a874ce25916
Victor Mendoza <victormendoza@animum3d.com>
2022-03-27 23:34:23 +0200

    He agregado al devicerepository el metodo de busqueda

--- src/BackOffice/Devices/Domain/DeviceRepository.php
+++ src/BackOffice/Devices/Domain/DeviceRepository.php
@@ -3,8 +3,11 @@
 namespace Vic\BackOffice\Devices\Domain;
 
 use Vic\BackOffice\Devices\Domain\Device;
+use Vic\BackOffice\Devices\Domain\ValueObject\DeviceId;
 
 interface DeviceRepository
 {
     public function save(Device $device): void;
+
+    public function search(DeviceId $id): ?Device;
 }
