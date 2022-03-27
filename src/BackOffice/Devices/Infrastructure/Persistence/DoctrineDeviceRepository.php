a5762dde21233d6201ed4a8de81dcb5009411fd2
Victor Mendoza <victormendoza@animum3d.com>
2022-03-27 23:35:36 +0200

    He agregado la la clase DoctrineDeviceRepository el metodo search

--- src/BackOffice/Devices/Infrastructure/Persistence/DoctrineDeviceRepository.php
+++ src/BackOffice/Devices/Infrastructure/Persistence/DoctrineDeviceRepository.php
@@ -6,7 +6,7 @@ namespace Vic\BackOffice\Devices\Infrastructure\Persistence;
 
 use Vic\BackOffice\Devices\Domain\Device;
 use Vic\BackOffice\Devices\Domain\DeviceRepository;
-use Vic\BackOffice\Devices\Domain\ValueObjet\DeviceId;
+use Vic\BackOffice\Devices\Domain\ValueObject\DeviceId;
 use Vic\Shared\Infrastructure\Persistence\Doctrine\DoctrineRepository;
 
 final class DoctrineDeviceRepository extends DoctrineRepository implements DeviceRepository
@@ -14,5 +14,10 @@ final class DoctrineDeviceRepository extends DoctrineRepository implements Devic
     public function save(Device $device): void
     {
         $this->persist($device);
+    }
+
+    public function search(DeviceId $id): ?Device
+    {
+       return $this->repository(Device::class)->find($id);
     }
 }
