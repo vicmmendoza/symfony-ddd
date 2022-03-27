9a6cc3f4fa8a4312fbf2234ba18e332d7e43283f
Victor Mendoza <victormendoza@animum3d.com>
2022-03-27 17:31:22 +0200

    He modificado los test para no permitir que se puedan crear Devices sin nombre ni mac

--- tests/Features/DevicePutControllerTest.php
+++ tests/Features/DevicePutControllerTest.php
@@ -2,19 +2,65 @@
 
 namespace App\Tests\Features;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
 class DevicePutControllerTest extends WebTestCase
 {
-    public function testPutDevices()
+
+    use ReloadDatabaseTrait;
+
+    public function testSuccess()
     {
         $client = static::createClient();
 
-        $client->request(   'PUT', 
-                            '/devices/90dc55b0-abc0-11ec-b909-0242ac120002', 
-                            [   "name"          =>  "Víctor Mendoza",
-                                "mac_address"   =>  "2B-45-B8-65-0B-7F"]);
+        $this->sendRequest($client, '90dc55b0-abc0-11ec-b909-0242ac120002', $this->getJson() );
 
-        $this->assertResponseStatusCodeSame(201);
+        $this->assertEquals(Response::HTTP_CREATED, $client->getResponse()->getStatusCode());
     }
+
+    public function testDataEmpty()
+    {
+        $client = static::createClient();
+
+        $this->sendRequest($client, '90dc55b0-abc0-11ec-b909-0242ac120002', [] );
+
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
+    }
+
+    public function testNameEmpty()
+    {
+        $client = static::createClient();
+
+        $this->sendRequest($client, '90dc55b0-abc0-11ec-b909-0242ac120002', $this->getJson('', '2B-45-B8-65-0B-7F') );
+
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
+    }
+
+    public function testMacAddressEmpty()
+    {
+        $client = static::createClient();
+
+        $this->sendRequest($client, '90dc55b0-abc0-11ec-b909-0242ac120002', $this->getJson('La maquina', '') );
+
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
+    }
+
+    private function sendRequest(KernelBrowser $client, string $uuid, array $json)
+    {
+        $client->request(
+            'PUT',
+            '/api/devices/'.$uuid,
+            $json
+        );
+    }
+
+    private function getJson(string $name = 'La maquina', string $mac_address = '2B-45-B8-65-0B-7F') : array
+    {
+        return [   "name"          =>  $name,
+                    "mac_address"   =>  $mac_address];
+    }
+
 }
