fa42838594834edfbfe0e3b70cdda24acb9dcd52
Victor Mendoza <victormendoza@animum3d.com>
2022-03-27 17:38:24 +0200

    He agregado un control de errores en el controller

--- app/Controller/Devices/DevicesPutController.php
+++ app/Controller/Devices/DevicesPutController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Vic\BackOffice\Devices\Application\DeviceCreator;
 use Vic\BackOffice\Devices\Application\CreateDeviceCommand;
 use Vic\BackOffice\Devices\Application\CreateDeviceCommandHandler;
-use Vic\BackOffice\Devices\Infrastructure\Persistence\DoctrineDeviceRepository;
 class DevicesPutController
 {
 
@@ -21,13 +20,17 @@ class DevicesPutController
     public function __invoke(string $id, Request $request): Response
     {
 
-        $this->command->__invoke(new CreateDeviceCommand(
-                                            $id,
-                                            $request->get('name'),
-                                            $request->get('mac_address'),
-                                        ));
-        
-        return new Response('', Response::HTTP_CREATED);
+        try {
+            $this->command->__invoke(new CreateDeviceCommand(
+                $id,
+                $request->get('name')??'',
+                $request->get('mac_address')??'',
+            ));
 
+            return new Response('', Response::HTTP_CREATED);    
+        } catch (\Exception $e) {
+            return new Response($e, Response::HTTP_BAD_REQUEST);    
+        }
     }
+
 }
